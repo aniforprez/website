748d8185b166454603938b5e892bea1bf14a2c94
Anirudh S <me@aniforprez.dev>
2022-10-27 15:49:35 +0530

    Minor formatting stuff

--- README.md
+++ README.md
@@ -4,8 +4,12 @@
 
 ### Requirements
 
-This site uses [zola](https://www.getzola.org/). Follow the [instructions](https://www.getzola.org/documentation/getting-started/installation/) for installing it to your platform of choice and run `zola serve` for the site to be served by default at `127.0.0.1:1111`
+This site uses [zola](https://www.getzola.org/). Follow the [instructions](https://www.getzola.org/documentation/getting-started/installation/)
+for installing it to your platform of choice and run `zola serve` for the site to be served by default at
+`127.0.0.1:1111`
 
-This site also uses tailwind which requires npm and node. Install node and npm through your process of choice. I prefer asdf to manage node and, consequently, npm.
+This site also uses tailwind which requires npm and node. Install node and npm through your process of choice. I prefer
+asdf to manage node and, consequently, npm.
 
-To serve the CSS files for development with Tailwind's JIT, run `npm run dev`. To build the CSS files for production, run `npm run build`
+To serve the CSS files for development with Tailwind's JIT, run `npm run dev`. To build the CSS files for production,
+run `npm run build`
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 module.exports = {
   content: [
     "./content/**/*.md",
+    "./static/js/*.js",
     "./content/**/*.html",
-    "./templates/**/*.html"
+    "./templates/**/*.html",
   ],
-  darkMode: 'class', // 'media' or 'class'
+  darkMode: "class", // 'media' or 'class'
   theme: {
     fontFamily: {
-      'sans': '"Quattrocento Sans", ui-sans-serif, system-ui, sans-serif',
-      'serif': '"Playfair Display", ui-serif, serif',
-      'mono': 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'
+      sans: '"Quattrocento Sans", ui-sans-serif, system-ui, sans-serif',
+      serif: '"Playfair Display", ui-serif, serif',
+      mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
     },
     extend: {
       colors: {
-        'primary': '#f0d6bb',
-        'secondary': '#353535',
-        'tertiary': '#a08a7f'
-      }
+        primary: "#f0d6bb",
+        secondary: "#353535",
+        tertiary: "#a08a7f",
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
