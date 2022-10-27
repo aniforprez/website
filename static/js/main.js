ee3a15c8f7342535726fcf20c347845bd68160e9
Anirudh S <me@aniforprez.dev>
2022-10-27 14:02:35 +0530

    Solidified base.html with dark mode switches

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,31 @@
-import setDarkMode from "./dark.js";
+const setDarkMode = (isItDark) => {
+  let theme = null;
+  if (isItDark === null || isItDark === undefined) {
+    theme = localStorage.getItem("theme");
+    if (theme === null) {
+      theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+    }
+  } else {
+    theme = isItDark ? "dark" : "light";
+  }
+
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    document.getElementById("light-toggle").classList.remove("hidden");
+    document.getElementById("dark-toggle").classList.add("hidden");
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+    document.getElementById("light-toggle").classList.add("hidden");
+    document.getElementById("dark-toggle").classList.remove("hidden");
+  }
+};
 
 const toggleDarkMode = (isItDark) => {
-    if (isItDark) {
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.setItem("theme", "not-dark");
-    }
-
-    setDarkMode();
+  setDarkMode(isItDark);
 };
+
+setDarkMode();
