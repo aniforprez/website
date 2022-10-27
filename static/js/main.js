d3950ebfd80e4761a56f4f1ac9ec86b6d7f2ad2c
Anirudh S <me@aniforprez.dev>
2022-10-27 16:39:15 +0530

    Minor fixes for dark mode

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,36 @@
-const setDarkMode = (isItDark) => {
-  let theme = null;
-  if (isItDark === null || isItDark === undefined) {
-    theme = localStorage.getItem("theme");
-    if (theme === null) {
-      theme = window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light";
-    }
-  } else {
-    theme = isItDark ? "dark" : "light";
+const getTheme = () => {
+  theme = localStorage.getItem("theme");
+  if (theme === null) {
+    theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
   }
+
+  return theme;
+};
+
+const setDarkMode = (isItDark) => {
+  const theme = isItDark ? "dark" : "light";
 
   if (theme === "dark") {
     document.documentElement.classList.add("dark");
     localStorage.setItem("theme", "dark");
-    document.getElementById("light-toggle").classList.remove("hidden");
-    document.getElementById("dark-toggle").classList.add("hidden");
+    document.getElementById("light-toggle").classList.remove("opacity-0");
+    document.getElementById("light-toggle").classList.add("z-10");
+    document.getElementById("dark-toggle").classList.add("opacity-0");
+    document.getElementById("dark-toggle").classList.remove("z-10");
   } else {
     document.documentElement.classList.remove("dark");
     localStorage.setItem("theme", "light");
-    document.getElementById("light-toggle").classList.add("hidden");
-    document.getElementById("dark-toggle").classList.remove("hidden");
+    document.getElementById("light-toggle").classList.add("opacity-0");
+    document.getElementById("light-toggle").classList.remove("z-10");
+    document.getElementById("dark-toggle").classList.remove("opacity-0");
+    document.getElementById("dark-toggle").classList.add("z-10");
   }
 };
 
-const toggleDarkMode = (isItDark) => {
-  setDarkMode(isItDark);
+const toggleDarkMode = () => {
+  setDarkMode(getTheme() !== "dark");
 };
 
-setDarkMode();
+setDarkMode(getTheme() === "dark");
