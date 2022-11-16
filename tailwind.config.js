32c7f905a7e1b4ab130a5b6c77d29c1f04c43c9b
Anirudh Sylendranath <ani.22691@gmail.com>
2022-11-16 13:18:17 +0530

    Minor styling changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,6 @@ module.exports = {
       serif: '"Playfair Display", ui-serif, serif',
       mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
     },
-    extend: {
-      colors: {
-        primary: "#f0d6bb",
-        secondary: "#353535",
-        tertiary: "#a08a7f",
-      },
-    },
   },
   variants: {
     extend: {},
