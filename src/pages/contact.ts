c5a3fcf77228ad2f9ef3fc5b5d7f762383a1474d
Anirudh Sylendranath <me@aniforprez.dev>
2025-06-04 16:25:56 +0530

    Send me an email when contacted

--- src/pages/contact.ts
+++ src/pages/contact.ts
@@ -1,23 +1,76 @@
 import type { APIRoute } from "astro";
+import { ActionError } from "astro:actions";
+import { Resend } from "resend";
+import { z } from "zod";
 
 export const prerender = false;
 
+const rateLimit = new Map();
+// Rate limit window in ms
+const windowMs = 24 * 60 * 60 * 1000;
+const limit = 1;
+
+const RESEND_API_KEY = import.meta.env.RESEND_API_KEY;
+
 export const POST: APIRoute = async ({ request }) => {
   try {
-    console.log();
-    const data = await request.formData();
-    const name = data.get("name");
-    const email = data.get("email");
-    const message = data.get("message");
+    const data = Object.fromEntries(await request.formData());
 
-    const honeypot = data.get("emai");
+    const schema = z.object({
+      name: z.string().min(3).max(100),
+      email: z.string().email().max(50),
+      message: z.string().min(10).max(1000),
+    });
+
+    const honeypot = data["email-confirm"];
     if (honeypot) {
       console.log("honeypotted", request);
       return new Response(JSON.stringify({ success: true, error: "" }));
     }
 
-    // Send an email to me
-    console.log(name, email, message);
+    const parsed = schema.safeParse(data);
+    if (!parsed.success) {
+      return new Response(
+        JSON.stringify({ success: true, error: parsed.error.message }),
+      );
+    }
+
+    // Rate limit if email gets sent
+    const ip = request.headers.get("x-forwarded-for") || "unknown";
+    const now = Date.now();
+    const userLimit = rateLimit.get(ip) || {
+      count: 0,
+      resetTime: now + windowMs,
+    };
+    console.log(rateLimit);
+    if (now > userLimit.resetTime) {
+      userLimit.count = 1;
+      userLimit.resetTime = now + windowMs;
+    } else {
+      userLimit.count++;
+    }
+
+    // Send an email to me if not rate limited
+    if (userLimit.count <= limit) {
+      const resend = new Resend(RESEND_API_KEY);
+
+      const { error } = await resend.emails.send({
+        from: "Finesse <dev@finesse.money>",
+        to: ["ani.22691@gmail.com"],
+        subject: parsed.data.name,
+        html: `From ${parsed.data.email}\n\n${parsed.data.message}`,
+      });
+
+      if (error) {
+        throw new ActionError({
+          code: "BAD_REQUEST",
+          message: error.message,
+        });
+      }
+    }
+
+    rateLimit.set(ip, userLimit);
+
     return new Response(JSON.stringify({ success: true, error: "" }));
   } catch (error) {
     let message = "Unknown error";
