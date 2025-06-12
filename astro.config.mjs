f8af23cf1b13816422f423752db00be3e643c704
Anirudh Sylendranath <me@aniforprez.dev>
2025-06-12 14:36:04 +0530

    Minor changes for caching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-
 import netlify from "@astrojs/netlify";
-
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 import expressiveCode from "astro-expressive-code";
+import { defineConfig } from "astro/config";
+import remarkSectionize from "remark-sectionize";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,10 +15,7 @@ export default defineConfig({
     "/blog/recommends": "/blog/recommends/1",
     "/blog/tags/[tag]": "/blog/tags/[tag]/1",
   },
-  markdown: {
-    remarkPlugins: ["remark-sectionize"],
-    shikiConfig: { theme: "slack-dark" },
-  },
+  markdown: { remarkPlugins: [remarkSectionize] },
 
   integrations: [
     expressiveCode({
@@ -48,7 +43,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
+  adapter: netlify(),
 });
